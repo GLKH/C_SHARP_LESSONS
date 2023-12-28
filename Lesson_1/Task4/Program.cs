755a419eb0aa3bf0251a8680c7c6d2ab471ece16
Galina Kharitonova <kharitonovagl@gmail.com>
2023-12-29 00:54:52 +0300

    lesson1:task4

---
+++ Lesson_1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 5;
+int b = 80;
+int c = 6;
+int d = 12;
+int e = 1;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
