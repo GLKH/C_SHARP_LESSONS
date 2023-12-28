82b92d9157fd9643668f4c3394f10cb19bb04b32
Galina Kharitonova <kharitonovagl@gmail.com>
2023-12-29 01:26:03 +0300

    add lesson1:HW

---
+++ Lesson_1/HW/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 50;
+int b = 85;
+int c = 10;
+int d = 15;
+
+int result = (a + b) / (c + d);
+Console.WriteLine(result);
+
+double e = 110;
+
+double result2 = (a + b) / (e + d);
+Console.WriteLine(result2);
+
+double f = 7;
+
+double result3 = (a + b) / (f + d);
+Console.WriteLine(result3);
