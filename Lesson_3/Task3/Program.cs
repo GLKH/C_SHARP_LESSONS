64ea428f28ccb7eceaf072cdd004cf87250bae42
Galina Kharitonova <kharitonovagl@gmail.com>
2024-01-17 22:51:05 +0300

    add Lesson3:Task3

---
+++ Lesson_3/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Создаем функцию, которая обнулить четные элементы массива
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+ZeroEvenElements(array);
+PrintArray(array);
